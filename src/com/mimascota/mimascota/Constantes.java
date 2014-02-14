3fa7ef87c6b0030520533ca265c660ede69280a1
Gonzalo Rodriguez <gonzalo89@ymail.com>
2014-02-14 11:54:21 -0300

    Agrego menu para seleccionar ip del server desde el login

--- src/com/mimascota/mimascota/Constantes.java
+++ src/com/mimascota/mimascota/Constantes.java
@@ -5,4 +5,7 @@ public class Constantes {
 	static String IPSERVER = "192.168.1.34";
 	static int viewerWidth = 256;
 	static int viewerHeight = 179;
+	public static void setIpServer(String string) {
+		IPSERVER = string;		
+	}
 }
--- src/com/mimascota/mimascota/MainActivity.java
+++ src/com/mimascota/mimascota/MainActivity.java
@@ -3,7 +3,10 @@ package com.mimascota.mimascota;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 
 public class MainActivity extends FragmentActivity implements LoginFragment.InterfaceLogin {
 	
@@ -50,6 +53,25 @@ public class MainActivity extends FragmentActivity implements LoginFragment.Inte
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
+		SubMenu smnu = menu.
+			    addSubMenu(Menu.NONE, 0, Menu.NONE, "Servidor");
+			//        .setIcon(android.R.drawable.ic_menu_agenda);
+		
+		for(int i = 2 ; i <=200 ; i++) {
+			smnu.add(Menu.NONE, i, Menu.NONE, "192.168.1." + Integer.toString(i));
+		}
+			
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.d("MiMascota", Integer.toString(item.getItemId()));
+		if(item.getItemId() > 0) {
+			Constantes.setIpServer("192.168.1." +Integer.toString(item.getItemId()));
+		}
+		
+	    return true;
 	}
 }
